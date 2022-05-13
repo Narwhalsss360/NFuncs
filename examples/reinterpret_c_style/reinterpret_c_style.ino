6f56f9c4b7acaf4317a00be1e324642505fb72a9
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-12 23:16:43 -0400

    added examples

--- examples/reinterpret_c_style/reinterpret_c_style.ino
+++ examples/reinterpret_c_style/reinterpret_c_style.ino
@@ -1,8 +1,22 @@
+//Only need the defenition for this example.
 #include <NDefs.h>
+
+//Declaring a byte array to change to a uint32_t/unsigned int
+byte byteArray[4] = { 0x41, 0xAE, 0x01, 0xF2 };
+
+//Declaring a uint32_t/unsigned int to get the byte array as an integer.
+uint32_t myNumber;
 
 void setup()
 {
-
+    //C-Style cast.
+    myNumber = *((unsigned long*)byteArray);
+    
+    //Changing values.
+    byteArray[3] = 0x00;
+    
+    //using defenition.
+    myNumber = reinterpret_c_style(byteArray);
 }
 
 void loop()
